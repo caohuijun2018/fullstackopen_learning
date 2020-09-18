74addd179d1ff760d652921fb5e568c123a853b1
chj <chj@promote.cache-dns.local>
2020-09-18 11:25:37 +0800

    显示过滤后的内容

--- reLearn-Fullstack/part2/note/src/App.js
+++ reLearn-Fullstack/part2/note/src/App.js
@@ -4,6 +4,7 @@ import Note from "./components/Note";
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes)
   const [newNote, setNewNote] = useState(' ')
+  const [showAll, setShowAll] = useState(true)
   const addNote = (event) => {
     event.preventDefault();
     const noteObject = {
@@ -15,6 +16,7 @@ const App = (props) => {
     setNotes(notes.concat(noteObject))
     setNewNote('')
   }
+  const noteToShow = showAll ? notes : notes.filter(note => note.important === true)
   const handleNoteChange = (event) => {
     console.log(event.target.value)
     setNewNote(event.target.value)
@@ -22,19 +24,24 @@ const App = (props) => {
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
       <ul>
-        {notes.map((note) => (
+        {noteToShow.map((note) => (
           <Note key={note.id} note={note} />
         ))}
       </ul>
-    
+
       <form onSubmit={addNote}>
         <input
           value={newNote}
           onChange={handleNoteChange}
         />
         <button type="submit">save</button>
-      </form>   
+      </form>
 
     </div>
   );
