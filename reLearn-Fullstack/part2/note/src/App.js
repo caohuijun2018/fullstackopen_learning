9b8f70746161f60a4fca56319906cd6abc020063
chj <chj@promote.cache-dns.local>
2020-09-20 15:37:37 +0800

    post数据到服务器

--- reLearn-Fullstack/part2/note/src/App.js
+++ reLearn-Fullstack/part2/note/src/App.js
@@ -1,20 +1,20 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Note from "./components/Note";
 import './App.css'
-import axios from'axios'
+import axios from 'axios'
 const App = (props) => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState(' ')
   const [showAll, setShowAll] = useState(true)
- useEffect (() => {
-  axios
-     .get('http://localhost:3001/notes')
-     .then(response => {
-       console.log('effect')
-       setNotes(response.data)
-     })
- },[]) //第二个参数为空数组，说明该组件只在第一次渲染的时候执行
-  
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('effect')
+        setNotes(response.data)
+      })
+  }, []) //第二个参数为空数组，说明该组件只在第一次渲染的时候执行
+
   const addNote = (event) => {
     event.preventDefault();
     const noteObject = {
@@ -23,8 +23,14 @@ const App = (props) => {
       date: new Date().toISOString,
       important: Math.random() < 0.5
     }
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+
+    axios
+      .post('http://localhost:3001/notes', noteObject)
+      .then(response => {
+        setNotes(notes.concat(response.data))
+        setNewNote('')
+      })
+
   }
   const noteToShow = showAll ? notes : notes.filter(note => note.important === true)
   const handleNoteChange = (event) => {
