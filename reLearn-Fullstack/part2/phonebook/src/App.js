b9e75936145740969d5bdaa3e393b84e7e64b25f
chj <chj@chjdeMacBook-Pro.local>
2020-09-21 19:32:13 +0800

    从后端get数据

--- reLearn-Fullstack/part2/phonebook/src/App.js
+++ reLearn-Fullstack/part2/phonebook/src/App.js
@@ -1,16 +1,20 @@
-import React, { useState } from "react";
+import React, { useState  , useEffect} from "react";
 import "./App.css";
+import axios from 'axios'
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [errorMessage, setErrorMessage] = useState(null);
   const [newPhone, setNewPhone] = useState("");
   const [filterName, setFilterName] = useState("");
+  useEffect(() => {
+    axios
+     .get('http://localhost:3001/persons')
+     .then(response => {
+       setPersons(response.data)
+       console.log(response.data)
+     })
+  },[])
   const Notification = ({ message }) => {
     if (message === null) {
       return null;
